c018be13cd3049bf5472ef51c4368fe59b09e752
Nithish <nithish.vemula@unisys.com>
2026-04-25 10:33:18 +0530

    Update changes

--- frontend/app.js
+++ frontend/app.js
@@ -323,7 +323,8 @@ document.getElementById('permitForm').addEventListener('submit', async function(
             
             showSuccessModal(
                 'Permit Application Created!',
-                'Your permit application has been created successfully.',
+                'Your permit application has been created successfully in qa2 console.',
+                'Please do complete work items/valuation items, LAHD questionnaires, Select APN, Capture-inperson declaration before going to complete submissions.',
                 `Application Number: ${data.applicationName}`
             );
             
